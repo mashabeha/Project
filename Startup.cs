0e91d67966bc0aa7249e6255d61e05f1bdd4bd32
mashabeha <73879302+mashabeha@users.noreply.github.com>
2020-12-03 10:42:10 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Data;
+using Shop.Data.Interfaces;
+using Shop.Data.mocks;
+using Shop.Data.Repository;
+using Shop.Data.Shoes;
+
+namespace Shop
+{
+    public class Startup
+    {
+        private IConfigurationRoot _confString;
+        public Startup(IHostingEnvironment hostEnv)
+        {
+            _confString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+
+        }
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confString.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IAllShoes, ShoesRepository>();
+            services.AddTransient<IShoesCategory, CategoryRepository>();
+            services.AddTransient<IAllOrders, OrdersRepository>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped(sp => ShopCart.GetCart(sp));
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddMvc().AddSessionStateTempDataProvider();
+            services.AddSession();
+
+
+     
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{controller=Home}/{ action=Index}/{id?}");
+
+                routes.MapRoute(name: "categoryFilter", template: "Product/{action}/{category?}", defaults: new { Controllers = "Shoes", action = "List" });
+            });
+           
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                AppDBContent content = scope.ServiceProvider.GetRequiredService<AppDBContent>();
+
+                DBObjects.Initial(content);
+            }
+
+        }
+    }
+}
